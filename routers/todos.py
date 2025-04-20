299ef8ae2233ffb86467416a6e7f489b4827f5bf
alfonWaz13 <alfonwaz13@icloud.com>
2025-04-20 11:56:45 +0200

    feat: add create todo endpoint

--- routers/todos.py
+++ routers/todos.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from source.database import get_db
 from source.models import ToDos
+from source.schemas.todo import CreateToDoRequest
 
 router = APIRouter(prefix="/todos", tags=["ToDos"])
 
@@ -18,3 +19,9 @@ async def read_todo(db: Session = Depends(get_db), todo_id: int = Path(gt=0)):
     if todo:
         return todo
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Item not found')
+
+@router.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: Session, request, todo_request: CreateToDoRequest):
+    todo_to_insert = ToDos(**todo_request.model_dump())
+    db.add(todo_to_insert)
+    db.commit()
