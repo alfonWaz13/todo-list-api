bee4d30e832d76c0d10ffe5fd94503daf7ad5697
alfonWaz13 <alfonwaz13@icloud.com>
2025-04-20 12:10:34 +0200

    feat: add update todo endpoint

--- source/schemas/todo.py
+++ source/schemas/todo.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
 
-class CreateToDoRequest(BaseModel):
+class ToDoRequest(BaseModel):
     title: str = Field(min_length=3)
     description: str
     priority: int = Field(gt=0, lt=3)
