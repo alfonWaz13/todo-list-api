46cb3952447f8ba2baed0f908ade2feec0871378
alfonWaz13 <alfonwaz13@icloud.com>
2025-04-20 11:50:10 +0200

    feat: add single todo get method

--- routers/todos.py
+++ routers/todos.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path, HTTPException
 from starlette import status
 from sqlalchemy.orm import Session
 
@@ -11,3 +11,10 @@ router = APIRouter(prefix="/todos", tags=["ToDos"])
 async def read_all(db: Session = Depends(get_db)):
     todos = db.query(ToDos).all()
     return todos
+
+@router.get("/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: Session = Depends(get_db), todo_id: int = Path(gt=0)):
+    todo = db.query(ToDos).filter(ToDos.id == todo_id).first()
+    if todo:
+        return todo
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Item not found')
