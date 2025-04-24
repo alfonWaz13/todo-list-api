cff47b9e488495615dca875946dfd1889e3d0eca
alfonWaz13 <alfonwaz13@icloud.com>
2025-04-24 10:23:19 +0200

    refactor: rename config.py to project_config.py

--- source/database.py
+++ source/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from config import Config
+from project_config import Config
 
 engine = create_engine(Config.DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
